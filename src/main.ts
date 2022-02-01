64796a28dea3e78def2ff227c77c640af3c099eb
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-02-01 04:47:02 +0400

    Add logging support

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
+import { LogLevel }    from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule }   from './app.module';
 import { env }         from './env';
 
+function toNestLogLevel(level: string): LogLevel
+{
+	switch(level)
+	{
+		case "trace":
+			return "verbose";
+		case "info":
+			return "log";
+		case "debug":
+			return "debug";
+		case "error":
+		case "fatal":
+			return "error";
+		case "warn":
+			return "warn";
+		default:
+			return "log";
+	}
+}
+
 async function bootstrap()
 {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		logger: [toNestLogLevel(env.LOG_LEVEL)]
+	});
 	await app.listen(env.PORT, env.HOST);
 }
 
