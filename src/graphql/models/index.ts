7378f745fb51958bdd5eb5ac5a8f40d6a31cbfb9
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-02-01 04:55:42 +0400

    Move scalars array to index

--- src/graphql/models/index.ts
+++ src/graphql/models/index.ts
@@ -1,3 +1,9 @@
+import {
+	AssociatedDiseaseCategoriesScalar,
+	AssociatedDiseasesScalar,
+}                     from './associated-diseases.model';
+import { TermScalar } from './gene-onthology.model';
+
 export * from './aging-mechanisms.model';
 export * from './associated-diseases.model';
 export * from './gene.model';
@@ -9,3 +15,10 @@ export * from './origin.model';
 export * from './researches.model';
 export * from './enums';
 export * from './options';
+export * from './orthologs.model';
+
+export const SCALARS = [
+	AssociatedDiseaseCategoriesScalar,
+	AssociatedDiseasesScalar,
+	TermScalar
+]
